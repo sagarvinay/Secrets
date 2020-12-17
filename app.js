8131d696e0fff69a93bdecb321ba8e2bafeeb8d5
Vinay sagar <vrs223@gmail.com>
2020-12-18 02:00:41 +0530

    Level 3 Authentication. Hashing. Add md5 package

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,7 +19,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -41,7 +40,7 @@ app.post("/register", function(req, res){
 
   const user = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   user.save(function(err){
@@ -56,7 +55,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email: username}, function(err, foundUser){
     if (!err) {
       if (foundUser) {
